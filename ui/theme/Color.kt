783ce6a7ffa011dc0ec9910c3a45c5429b939b50
thabede25 <mndenithabede2@gmail.com>
2025-10-31 16:57:11 +0200

    Create Color.kt

---
+++ ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.yourname.freetranscriber.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF006A6D)
+val md_theme_light_secondary = Color(0xFF4A6364)
+val md_theme_light_background = Color(0xFFF6FFFD)
+
+val md_theme_dark_primary = Color(0xFF57D8DC)
+val md_theme_dark_secondary = Color(0xFFB1CCCD)
+val md_theme_dark_background = Color(0xFF001F22)
